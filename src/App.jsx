966db1c17688e3df7c7968cd52d49ef8036f3b60
Saad Khan <saadnadeem885@gmail.com>
2023-03-07 01:42:57 -0800

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,63 @@
-import { useState } from 'react';
-import './App.css';
-import BarChart from './components/barChart';
-import LineChart from './components/lineChart';
-import PieChart from './components/pieChart';
-import { UserData } from './data';
+import { useEffect, useState } from "react";
+import "./App.css";
+import BarChart from "./components/barChart";
+import LineChart from "./components/lineChart";
+import PieChart from "./components/pieChart";
 
 function App() {
-  const [userData1, setUserData1] = useState({
-    labels: UserData.map((d) => d.year),
-    datasets: [
-      {
-        label: 'User Gained',
-        data: UserData.map((d) => d.userGain),
-        backgroundColor: ['green'],
-        borderColor: ['black'],
-        borderWidth: 2,
-      },
-      {
-        label: 'User Lost',
-        data: UserData.map((d) => d.userLost),
-        backgroundColor: ['blue'],
-        borderColor: ['black'],
-        borderWidth: 2,
-      },
-    ],
-  });
-  const [userData2, setUserData2] = useState({
-    labels: UserData.map((d) => d.year),
-    datasets: [
-      {
-        label: 'User Gained',
-        data: UserData.map((d) => d.userGain),
-        backgroundColor: ['green','blue','pink','yellow','purple'],
-        borderColor: ['black'],
-        borderWidth: 2,
+  const [chartData, setChartData] = useState();
+
+  useEffect(() => {
+    async function Fetch() {
+      const res = await fetch("http://localhost:3000/UserData");
+      if (!res.ok) {
+        throw new Error(`HTTP Error ${res.status} not found`);
       }
-    ],
-  });
+      const resData = await res.json();
+      const data = await resData;
+      const year = [];
+      const userGain = [];
+      const userLost = [];
+      data.map((d) => {
+        year.push(d.year)
+        userGain.push(d.userGain)
+        userLost.push(d.userLost)
+      })
+      setChartData({
+        labels: year,
+        datasets: [
+          {
+            label: "User Gained",
+            data: userGain,
+            backgroundColor: ["green"],
+            borderColor: ["black"],
+            borderWidth: 2,
+          },
+          {
+            label: "User Lost",
+            data: userLost,
+            backgroundColor: ["pink"],
+            borderColor: ["black"],
+            borderWidth: 2
+          }
+        ],
+      });
+    }
+
+    Fetch();
+  }, []);
 
   return (
-    <div className='div'>
-      <div className='main'>
-        <BarChart chartData={userData1} />
+    <div className="div">
+      <div className="main">
+        {chartData && <BarChart chartData={chartData} />}
       </div>
-      <div className='main'>
-        <LineChart chartData={userData1} />
+      <div className="main">
+        {chartData && <LineChart chartData={chartData}/>}
       </div>
+
       <div className='main'>
-        <PieChart chartData={userData2} />
+        {chartData && <PieChart chartData={chartData} />}
       </div>
     </div>
   );
